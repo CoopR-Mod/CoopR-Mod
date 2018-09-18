0fa29bd0a8b121b92d1d9025bf9f51d76f0552a3
xetra11 <xetra11@outlook.com>
2018-09-18 14:23:35 +0200

    refactor(constants): add new constants

--- constants.hpp
+++ constants.hpp
@@ -1,9 +1,12 @@
 #define DEBUG_STR_SERVER_INIT "SERVER INIT"
 #define DEBUG_STR_CLIENT_INIT "CLIENT INIT"
+#define DEBUG_STR_SERVER_SIDE "SERVER"
+#define DEBUG_STR_CLIENT_SIDE "CLIENT"
 #define DEBUG_STR_NPC_INIT "NPC INIT"
 #define DEBUG_STR_EVENT "EVENT"
 #define DEBUG_STR_PLAYER_ACTION "PLAYER ACTION"
-
 #define REP_PER_DOGTAG 5
 
 #define EMPTY_LOADOUT [[],[],[],[],[],[],"","",[],["","","","","",""]]
+
+#define KEY_PLAYER_PROFILES "dpl_player_profiles"

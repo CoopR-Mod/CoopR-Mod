f87a30526f3cb2e38df6bab0d90615731aa936b6
xetra11 <xetra11@outlook.com>
2018-09-18 21:21:01 +0200

    constants(intel): add whitelist for intel items

--- EventHandlers/constants.hpp
+++ EventHandlers/constants.hpp
@@ -1,1 +1,1 @@
-#define INTEL_ITEMS_WHITELIST ["ItemMap", "ItemRadio"];
+#define INTEL_ITEMS_WHITELIST ["CUP_item_Kostey_photos", "CUP_item_Cobalt_File", "CUP_item_Kostey_notebook", "CUP_item_Moscow_Bombing_File"];

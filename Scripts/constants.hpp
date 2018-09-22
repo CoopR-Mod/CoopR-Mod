989309194430de3cc33a93116ae3ff59dc05083c
xetra11 <xetra11@outlook.com>
2018-09-22 19:33:41 +0200

    refactor(constants): remove old constant

--- Scripts/constants.hpp
+++ Scripts/constants.hpp
@@ -1,6 +1,4 @@
 #define EMPTY_LOADOUT [[],[],[],[],[],[],"","",[],["","","","","",""]]
 #define KEY_REPUTATION "reputation"
 
-#define KEY_PLAYER_LOGGEDIN "dpl_is_logged_in"
-
 #define SYNC_RATE_MIN 0.2

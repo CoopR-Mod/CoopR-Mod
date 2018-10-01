ae1584ae32ebaba7f446886bd80370f5f5183ece
xetra11 <xetra11@outlook.com>
2018-10-01 20:32:31 +0200

    macro(debug): add debugging macro

--- macros.hpp
+++ macros.hpp
@@ -1,3 +1,4 @@
+#define LDEBUG(var1,var2) [format ['DEBUG - var1: %1', var2], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
 #define DEBUG(var1) [format ["DEBUG: %1", var1], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
 #define SLOG(var1) [var1, DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
 #define FLOG(var1,var2) [format [var1, var2], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug;

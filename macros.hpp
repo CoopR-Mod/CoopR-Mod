5a19937cb085ca92ea300c2e4eccf02d1561cac3
xetra11 <xetra11@outlook.com>
2018-10-01 19:22:11 +0200

    macro(debug): add logging macros

--- macros.hpp
+++ macros.hpp
@@ -1,1 +1,5 @@
 #define DEBUG(var1) [format ["DEBUG: %1", var1], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
+#define SLOG(var1) [var1, DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
+#define FLOG(var1,var2) [format [var1, var2], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug;
+
+

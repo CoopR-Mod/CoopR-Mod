4a6c177102e9947069be67c1315969cc6e9056f2
xetra11 <xetra11@outlook.com>
2018-11-27 23:58:43 +0100

    refactor(module): remove unneeded fields

--- addons/init/CfgVehicles.hpp
+++ addons/init/CfgVehicles.hpp
@@ -6,12 +6,6 @@ class CfgVehicles
         class AttributesBase
         {
             class Default;
-            class Edit; // Default edit box (i.e., text input field)
-            class Combo; // Default combo box (i.e., drop-down menu)
-            class Checkbox; // Default checkbox (returned value is Bool)
-            class CheckboxNumber; // Default checkbox (returned value is Number)
-            class ModuleDescription; // Module description
-            class Units; // Selection of units on which the module is applied
         };
         // Description base classes, for more information see below
         class ModuleDescription

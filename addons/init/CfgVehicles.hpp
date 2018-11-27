7a07bf286c0df52fa7ab2577523151b3e0c82394
xetra11 <xetra11@outlook.com>
2018-11-28 00:00:03 +0100

    refactor(module): remove comments

--- addons/init/CfgVehicles.hpp
+++ addons/init/CfgVehicles.hpp
@@ -6,6 +6,7 @@ class CfgVehicles
         class AttributesBase
         {
             class Default;
+            class ModuleDescription;
         };
         // Description base classes, for more information see below
         class ModuleDescription
@@ -15,26 +16,17 @@ class CfgVehicles
     };
     class CoopR_ModuleSetupCommander: Module_F
     {
-        // Standard object definitions
-        scope = 2; // Editor visibility; 2 will show it in the menu, 1 will hide it.
-        displayName = "Commander Module"; // Name displayed in the menu
+        scope = 2;
+        displayName = "Commander Module";
         //icon = "\myTag_addonName\data\iconNuke_ca.paa"; // Map icon. Delete this entry to use the default icon
         category = "CoopR_Setup";
-
-        // Name of function triggered once conditions are met
         function = "coopr_fnc_setupCommanderModule";
-        // Execution priority, modules with lower number are executed first. 0 is used when the attribute is undefined
         functionPriority = 1;
-        // 0 for server only execution, 1 for global execution, 2 for persistent global execution
         isGlobal = 1;
-        // 1 for module waiting until all synced triggers are activated
         isTriggerActivated = 1;
-        // 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
         isDisposable = 1;
-        // // 1 to run init function in Eden Editor as well
         is3DEN = 0;
 
-        // Module description. Must inherit from base class, otherwise pre-defined entities won't be available
         class ModuleDescription: ModuleDescription
         {
             description = "Sync this module with an AI unit to make it the CoopR commander unit"; // Short description, will be formatted as structured text

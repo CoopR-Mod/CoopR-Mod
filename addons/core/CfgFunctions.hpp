997654441bdf20e5f59a67de7bebaa4d28da9c95
xetra11 <xetra11@outlook.com>
2018-11-27 17:36:06 +0100

    refactor(debug): remove debug function

--- addons/core/CfgFunctions.hpp
+++ addons/core/CfgFunctions.hpp
@@ -2,7 +2,6 @@ class CfgFunctions {
     class coopr {
         class coopr_core_functions {
             file = "x\coopr\addons\core\functions";
-            class debug {};
             class sync {};
             class syncPlayersToServer {};
             class syncPlayerToServer {};
--- addons/core/debug.hpp
+++ addons/core/debug.hpp
@@ -1,3 +1,4 @@
+#define DEBUG_CFG [true, true, true]
 #define LDEBUG(var1,var2) [format ["DEBUG - %1: %2", var1, var2], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
 #define DEBUG(var1) [format ["DEBUG: %1", var1], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug
 

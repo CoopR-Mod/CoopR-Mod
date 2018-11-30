e799660fc4b581cacccc6f28a2b347bcb7ea705a
xetra11 <xetra11@outlook.com>
2018-11-30 10:40:14 +0100

    chore(build): create build script for *nix

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ARMAKE="tools/armake";
+DEST="@CoopR/addons"
+
+rm -rf @CoopR
+mkdir -p $DEST
+
+$ARMAKE build -p -f addons/character $DEST/character.pbo
+$ARMAKE build -p -f addons/core $DEST/core.pbo
+$ARMAKE build -p -f addons/death $DEST/death.pbo
+$ARMAKE build -p -f addons/equipment $DEST/equipment.pbo
+$ARMAKE build -p -f addons/eventhandler $DEST/eventhandler.pbo
+$ARMAKE build -p -f addons/init $DEST/init.pbo
+$ARMAKE build -p -f addons/integration $DEST/integration.pbo
+$ARMAKE build -p -f addons/intel $DEST/intel.pbo
+$ARMAKE build -p -f addons/lobby $DEST/lobby.pbo
+$ARMAKE build -p -f addons/prison $DEST/prison.pbo
+$ARMAKE build -p -f addons/reputation $DEST/reputation.pbo
+
+cp mod.cpp @CoopR/mod.cpp

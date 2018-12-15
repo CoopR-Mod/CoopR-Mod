c0a5cc7d4c13fc2461e7012a63839d56234ab56a
xetra11 <xetra11@outlook.com>
2018-12-15 16:06:07 +0100

    chore(dependency): add dep to init addon to all addons

--- addons/integration/config.cpp
+++ addons/integration/config.cpp
@@ -11,6 +11,7 @@ class CfgPatches {
         requiredVersion = 1.80;
         requiredAddons[] = {
             "coopr_core",
+            "coopr_init"
         };
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content unlocking.
         units[] = {};

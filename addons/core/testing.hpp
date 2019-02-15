da06eef87b62eb0d45a64d94ced8e115e4c7b845
xetra11 <xetra11@outlook.com>
2019-02-15 15:42:22 +0100

    fix(core): fix test macros

--- addons/core/testing.hpp
+++ addons/core/testing.hpp
@@ -3,10 +3,10 @@
 #define TSTART [format ["<< Test Start for %1 >>", DEBUG_CTX], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug;
 #define TEND [format ["<< Test End for %1 >>", DEBUG_CTX], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug;
 #define TLOG(var1) [format ["TEST - %1", var1], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug
-#define TSUCCESS [format ["TEST - failed", var1], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug
-#define TFAIL [format ["TEST - success", var1], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug
+#define TSUCCESS [format ["TEST - success", var1], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug
+#define TFAIL [format ["TEST - failure", var1], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug
 #define TEST(ctx) private _testContext = ctx
 
-#define TASSERT(expression) if(expression) then { [format ["TEST %1 - success", _textContext], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug; } else { [format ["TEST %1 - failure", _textContext], DEBUG_CTX, DEBUG_CFG] call CBA_fnc_debug; }
+#define TASSERT(expression) if(expression) then { [format ["TEST %1 - success", _testContext], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug; } else { [format ["TEST %1 - failure", _testContext], DEBUG_CTX, TEST_CFG] call CBA_fnc_debug; }
 
 

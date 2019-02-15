7cda6d451b863ddcbbaceb8e144f355a9e7ce0ae
xetra11 <xetra11@outlook.com>
2019-02-15 23:28:17 +0100

    refactor(globals): reorder globals
    COOPR-76

--- addons/core/globals.hpp
+++ addons/core/globals.hpp
@@ -1,7 +1,8 @@
+// System Globals
 #define EXEC_CLIENTS -2
 #define EXEC_SERVER  2
 #define EXEC_GLOBAL  0
-
+#define EMPTY_HASH ([[],[]] call CBA_fnc_hashCreate)
 
 // NEW CHARACTER
 #define STARTING_MONEY 500
@@ -15,9 +16,14 @@
 #define MP_MENU 49
 #define ABORT_BUTTON 104
 
+// Campaign Params
+#define COOPR_KEY_INTEL_PROGRESS "coopr_intel_progess"
+
+// Character Params
+#define EMPTY_LOADOUT [[],[],[],[],[],[],"","",[],["","","","","",""]]
+
 #define COOPR_KEY_PLAYER_PROFILES "coopr_player_profiles"
 #define COOPR_KEY_PLAYER_LOGGEDIN "coopr_login_state"
-#define COOPR_KEY_INTEL_PROGRESS "coopr_intel_progess"
 #define COOPR_KEY_TEAMKILLS "coopr_player_teamkills"
 
 #define COOPR_KEY_SLOT "coopr_character_slot"
@@ -31,6 +37,31 @@
 #define COOPR_KEY_POSITION "coopr_character_position"
 #define COOPR_KEY_LOADOUT "coopr_character_loadout"
 #define COOPR_KEY_WOUNDED_TIMESTAMP "coopr_character_wounded_timestamp"
+
+#define COOPR_STATE_OK "coopr_character_state_ok"
+#define COOPR_STATE_WIA "coopr_character_state_wia"
+#define COOPR_STATE_KIA "coopr_character_state_kia"
+
+#define COOPR_ROLE_LEADER "coopr_role_leader"
+#define COOPR_ROLE_MEDIC "coopr_role_medic"
+#define COOPR_ROLE_ENGINEER "coopr_role_engineer"
+#define COOPR_ROLE_DMR "coopr_role_marksman"
+#define COOPR_ROLE_MG "coopr_role_mg"
+#define COOPR_ROLE_NONE "coopr_role_none"
+
+// Recon Report Params
+#define COOPR_STRENGTH_TYPE_FIRETEAM "Fireteam"
+#define COOPR_STRENGTH_TYPE_SQUAD "Squad"
+#define COOPR_STRENGTH_TYPE_PLATOON "Platoon"
+#define COOPR_STRENGTH_TYPE_COMPANY "Company"
+
+#define COOPR_TASK_BEHAVIOUR_STATIC "Static"
+#define COOPR_TASK_BEHAVIOUR_MOVING "Moving"
+#define COOPR_TASK_BEHAVIOUR_PATROLING "Patroling"
+#define COOPR_TASK_BEHAVIOUR_DEFENDING "Defending"
+#define COOPR_TASK_BEHAVIOUR_ATTACKING "Attacking"
+
+// Task Globals
 #define COOPR_KEY_ACTIVE_TASK "coopr_character_active_task"
 
 #define COOPR_KEY_TASK_UID "coopr_task_uid"
@@ -45,11 +76,6 @@
 #define COOPR_TASK_TYPE_JTAC "coopr_task_type_jtac"
 #define COOPR_TASK_TYPE_AMBUSH "coopr_task_type_ambush"
 
-#define COOPR_STRENGTH_TYPE_FIRETEAM "fireteam"
-#define COOPR_STRENGTH_TYPE_SQUAD "squad"
-#define COOPR_STRENGTH_TYPE_PLATOON "platoon"
-#define COOPR_STRENGTH_TYPE_COMPANY "company"
-
 #define COOPR_STRENGTH_TYPE_FIRETEAM_MIN 1
 #define COOPR_STRENGTH_TYPE_FIRETEAM_MAX 4
 #define COOPR_STRENGTH_TYPE_SQUAD_MIN 8
@@ -59,18 +85,6 @@
 #define COOPR_STRENGTH_TYPE_COMPANY_MIN 80
 #define COOPR_STRENGTH_TYPE_COMPANY_MAX 150
 
-#define COOPR_STATE_OK "coopr_character_state_ok"
-#define COOPR_STATE_WIA "coopr_character_state_wia"
-#define COOPR_STATE_KIA "coopr_character_state_kia"
 
-#define EMPTY_HASH ([[],[]] call CBA_fnc_hashCreate)
-#define EMPTY_LOADOUT [[],[],[],[],[],[],"","",[],["","","","","",""]]
-
-#define COOPR_ROLE_LEADER "coopr_role_leader"
-#define COOPR_ROLE_MEDIC "coopr_role_medic"
-#define COOPR_ROLE_ENGINEER "coopr_role_engineer"
-#define COOPR_ROLE_DMR "coopr_role_marksman"
-#define COOPR_ROLE_MG "coopr_role_mg"
-#define COOPR_ROLE_NONE "coopr_role_none"
 
 

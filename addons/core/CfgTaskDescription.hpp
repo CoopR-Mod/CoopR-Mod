123d9d6a46b25156a90930d4a3798a721cedb98e
xetra11 <xetra11@outlook.com>
2019-03-08 16:12:59 +0100

    feat(tasks): add generation for asymmetric and assault task

--- addons/core/CfgTaskDescription.hpp
+++ addons/core/CfgTaskDescription.hpp
@@ -15,4 +15,35 @@ class CfgTaskDescriptions
         title = $STR_coopr_subtask_optional_recon_title;
         description = $STR_coopr_subtask_optional_recon_description;
     };
+    class coopr_task_type_sniperteam
+    {
+        title = $
+        STR_coopr_task_type_sniperteam_title;
+        description = $STR_coopr_task_type_sniperteam_description;
+    };
+    class coopr_task_type_asymmetric
+    {
+        title = $STR_coopr_task_type_asymmetric_title;
+        description = $STR_coopr_task_type_asymmetric_description;
+    };
+    class coopr_task_type_assault
+    {
+        title = $STR_coopr_task_type_assault_title;
+        description = $STR_coopr_task_type_assault_description;
+    };
+    class coopr_task_type_conquer
+    {
+        title = $STR_coopr_task_type_conquer_title;
+        description = $STR_coopr_task_type_conquer_description;
+    };
+    class coopr_task_type_jtac
+    {
+        title = $STR_coopr_task_type_jtac_title;
+        description = $STR_coopr_task_type_jtac_description;
+    };
+    class coopr_task_type_ambush
+    {
+        title = $STR_coopr_task_type_ambush_title;
+        description = $STR_coopr_task_type_ambush_description;
+    };
 };

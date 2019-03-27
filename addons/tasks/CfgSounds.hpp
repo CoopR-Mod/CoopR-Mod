dfe60f402f1bd7344dc3670be795cc5bbda9fb8a
xetra11 <xetra11@outlook.com>
2019-03-27 12:36:09 +0100

    feat(tasks): add new sound for erasing entries
    COOPR-73

--- addons/tasks/CfgSounds.hpp
+++ addons/tasks/CfgSounds.hpp
@@ -7,4 +7,10 @@ class CfgSounds
         sound[] = { "x\coopr\addons\tasks\data\sounds\paperfold.ogg", 0.8, 1, 100 };
         titles[] = { 0, "*paper folding*" };
     };
+    class coopr_sound_pencil_erasing
+    {
+        name = "Pencil_Erasing";
+        sound[] = { "x\coopr\addons\tasks\data\sounds\pencil_erasing.ogg", 0.8, 1, 100 };
+        titles[] = { 0, "*erasing entry*" };
+    };
 };

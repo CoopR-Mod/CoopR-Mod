4b08799b8db1ac1db291e2c381c82431ca3ee07f
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-04-02 00:17:55 -0300

    feat(campsite): Add the editor module function
    COOPR-93

---
+++ addons/campsite/cfgFunctions.hpp
@@ -0,0 +1,6 @@
+class CfgFunctions {
+    class coopr {
+        class coopr_campsite_functions {
+            file = "x\coopr\addons\campsite\functions";
+            class setupCampsiteModule{};
+};

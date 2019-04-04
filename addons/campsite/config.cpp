dc72fa8cce90dea6b22914a5dbbea9f47e2f723c
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-04-04 12:02:30 -0300

    feat(campsite): First iteration of the config.cpp
    COOPR-93

--- addons/campsite/config.cpp
+++ addons/campsite/config.cpp
@@ -10,10 +10,13 @@ class CfgPatches {
 
         requiredVersion = 1.80;
         requiredAddons[] = {
-            "coopr_core" // all addons need to depend on core
+            "coopr_core",
+            "Extended_Eventhandlers" // all addons need to depend on core
         };
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content unlocking.
-        units[] = {};
+        units[] = {
+            "coopr_item_camping"
+        };
         // List of weapons (CfgWeapons classes) contained in the addon.
         weapons[] = {
             "coopr_item_camping"
@@ -21,5 +24,7 @@ class CfgPatches {
     };
 };
 
+#include "constants.hpp"
+#include "cfgFunctions.hpp"
 #include "cfgWeapons.hpp"
 #include "cfgVehicles.hpp"

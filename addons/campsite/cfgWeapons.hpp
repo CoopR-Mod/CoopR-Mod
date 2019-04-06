c37cfc90bc4714dd4faa5160b82c57e4db9871ad
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-04-06 04:29:37 -0300

    feat(campsite): added localization for item name
    COOPR-93

--- addons/campsite/cfgWeapons.hpp
+++ addons/campsite/cfgWeapons.hpp
@@ -5,11 +5,11 @@ class cfgWeapons{
 	class coopr_item_camping : ItemCore{
 		scope = 2;
 		author = "Gilles";
-		displayName = "camping item"; // TODO-LOCALIZE
+		displayName = "$STR.coopr.campsite.item.campsite";
 		descriptionShort = "useless";
 		descriptionUse = "useless";
-		picture = "\A3\Weapons_F\Data\UI\gear_item_compass_ca.paa";
-		model = "\a3\Weapons_F\Ammo\mag_compass.p3d";
+		picture = "\A3\Weapons_F\Items\data\UI\gear_Toolkit_CA.paa";
+		model = "\A3\Structures_F\Civ\Camping\Campfire_F.p3d";
 
 		class ItemInfo: CBA_MiscItem_ItemInfo{
 			mass = 0.5;

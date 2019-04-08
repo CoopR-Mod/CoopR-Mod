4be673c56b39dd5737af2f7fae573a3efe190c89
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-04-08 15:35:59 -0400

    refactor(campsite): separate fnc in categories
    COOPR-93

--- addons/campsite/cfgFunctions.hpp
+++ addons/campsite/cfgFunctions.hpp
@@ -4,11 +4,18 @@ class CfgFunctions {
             file = "x\coopr\addons\campsite\functions";
             class setupCampsiteModule{};
         };
+
+        class coopr_campsite_helper{
+            file = "x\coopr\addons\campsite\helper";
+            class createMarkerUniqueName {};
+            class createMarkerArea {};
+            class canBuildCampsite {};
+            class isInsideCampingArea {};
+        };
         
         class coopr_campsite_functions{
             file = "x\coopr\addons\campsite\functions";
             class addCampsiteItemEH {};
-            class canBuildCampsite {};
             class buildCampsite {};
         };
     };        
--- addons/campsite/helper/fn_canBuildCampsite.sqf
+++ addons/campsite/helper/fn_canBuildCampsite.sqf
@@ -22,9 +22,11 @@ params["_structure"];
 switch _structure do{
 	case 0: {
 		if(!(player getVariable ["coopr_campingItem_0", false])) exitWith {false};
-		if(player inArea COOPR_HQ_WEST_BOUNDS) exitWith{false};
 		if(!isFormationLeader player) exitWith{false};
-		DEBUG("Player has the item, is outside of the HQ area and is a squad leader. Player can build");
 		true;
 	};
+	case 1: {
+		private _condition = call coopr_fnc_isInsideCampingArea;
+		_condition;
+		};
 };

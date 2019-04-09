65e7874d0cbdd0681456753a0f883b2bca959f50
xetra11 <xetra11@outlook.com>
2019-04-09 21:26:57 +0200

    refactor(tasks): small adjustments
    COOPR-100

--- addons/tasks/ui/CoopR_TaskBoard_Dialog.hpp
+++ addons/tasks/ui/CoopR_TaskBoard_Dialog.hpp
@@ -18,9 +18,9 @@ class CoopR_TaskBoard_Dialog
 	};
 	class Controls
 	{
-		class CoopR_TaskBoard_Document_Combat : ctrlButtonPictureOnly
+		class CoopR_TaskBoard_Document_Recon : ctrlButtonPictureOnly
 		{
-			idc = GUI_ID_TASKBOARD_DOCUMENT_COMBAT;
+			idc = GUI_ID_TASKBOARD_DOCUMENT_RECON;
 			x = safeZoneX + safeZoneW * 0.28125;
 			y = safeZoneY + safeZoneH * 0.31111112;
 			w = safeZoneW * 0.201875;
@@ -28,9 +28,9 @@ class CoopR_TaskBoard_Dialog
 			text = "x\coopr\addons\tasks\data\images\taskboard_recon.paa";
 
 		};
-		class CoopR_TaskBoard_Document_Recon : ctrlButtonPictureOnly
+		class CoopR_TaskBoard_Document_Combat : ctrlButtonPictureOnly 
 		{
-			idc = GUI_ID_TASKBOARD_DOCUMENT_RECON;
+			idc = GUI_ID_TASKBOARD_DOCUMENT_COMBAT;
 			x = safeZoneX + safeZoneW * 0.495;
 			y = safeZoneY + safeZoneH * 0.29666667;
 			w = safeZoneW * 0.22125;
@@ -38,39 +38,39 @@ class CoopR_TaskBoard_Dialog
 			text = "x\coopr\addons\tasks\data\images\taskboard_combat.paa";
 
 		};
-        class CoopR_TaskBoard_Document_Combat_Label
-        {
-            type = 0;
-            idc = GUI_ID_TASKBOARD_DOCUMENT_COMBAT_LABEL;
-            x = safeZoneX + safeZoneW * 0.556875;
-            y = safeZoneY + safeZoneH * 0.41888889;
-            w = safeZoneW * 0.0625;
-            h = safeZoneH * 0.03666667;
-            style = 0;
-            text = "Combat Tasks";
-            colorBackground[] = {0.6353,0.3569,0.2627,0};
-            colorText[] = {0,0,0,1};
-            font = "PuristaMedium";
-            sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+		class CoopR_TaskBoard_Document_Combat_Label
+		{
+			type = 0;
+			idc = GUI_ID_TASKBOARD_DOCUMENT_COMBAT_LABEL;
+			x = safeZoneX + safeZoneW * 0.556875;
+			y = safeZoneY + safeZoneH * 0.41888889;
+			w = safeZoneW * 0.0675;
+			h = safeZoneH * 0.03666667;
+			style = 0;
+			text = "Combat Tasks";
+			colorBackground[] = {0.6353,0.3569,0.2627,0};
+			colorText[] = {0,0,0,1};
+			font = "PuristaMedium";
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 
-        };
-        class CoopR_TaskBoard_Document_Recon_Label
-        {
-            type = 0;
-            idc = GUI_ID_TASKBOARD_DOCUMENT_RECON_LABEL;
-            x = safeZoneX + safeZoneW * 0.348125;
-            y = safeZoneY + safeZoneH * 0.39222223;
-            w = safeZoneW * 0.0625;
-            h = safeZoneH * 0.03666667;
-            style = 0;
-            text = "Recon Tasks";
-            colorBackground[] = {0.6353,0.3569,0.2627,0};
-            colorText[] = {0,0,0,1};
-            font = "PuristaMedium";
-            sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+		};
+		class CoopR_TaskBoard_Document_Recon_Label
+		{
+			type = 0;
+			idc = GUI_ID_TASKBOARD_DOCUMENT_RECON_LABEL;
+			x = safeZoneX + safeZoneW * 0.348125;
+			y = safeZoneY + safeZoneH * 0.39222223;
+			w = safeZoneW * 0.0625;
+			h = safeZoneH * 0.03666667;
+			style = 0;
+			text = "Recon Tasks";
+			colorBackground[] = {0.6353,0.3569,0.2627,0};
+			colorText[] = {0,0,0,1};
+			font = "PuristaMedium";
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 
-        };
-
+		};
 
 	};
+	
 };
--- addons/tasks/ui/fn_showTaskBoardDialog.sqf
+++ addons/tasks/ui/fn_showTaskBoardDialog.sqf
@@ -9,7 +9,9 @@ DEBUG("initialising taskboard ui");
 
 private _taskBoardDisplay = findDisplay GUI_ID_TASKBOARD_DIALOG;
 private _combatDocument = _taskBoardDisplay displayCtrl GUI_ID_TASKBOARD_DOCUMENT_COMBAT;
+private _combatLabel = _taskBoardDisplay displayCtrl GUI_ID_TASKBOARD_DOCUMENT_COMBAT_LABEL;
 private _reconDocument = _taskBoardDisplay displayCtrl GUI_ID_TASKBOARD_DOCUMENT_RECON;
+private _reconLabel = _taskBoardDisplay displayCtrl GUI_ID_TASKBOARD_DOCUMENT_RECON_LABEL;
 
 _reconDocument ctrlAddEventHandler ["MouseButtonDown", { DEBUG("Recon Document opened"); }];
 

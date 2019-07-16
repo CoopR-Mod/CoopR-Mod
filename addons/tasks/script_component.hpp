69c97db5f2fb0147eba709410c27f558b6f248b1
xetra11 <xetra11@outlook.com>
2019-07-16 14:32:00 +0200

    refactor(tasks): hide patrol option until it is implemented

--- addons/tasks/script_component.hpp
+++ addons/tasks/script_component.hpp
@@ -11,6 +11,7 @@
 // Recon Report Options
 #define COOPR_RECONREP_TYPE_OPTIONS ["Infantry", "Motorized", "Mechanized", "Armored"]
 #define COOPR_RECONREP_STRENGTH_OPTIONS ["Fireteam", "Squad", "Platoon", "Company"]
-#define COOPR_RECONREP_BEHAVIOUR_OPTIONS ["Patrol", "Combat", "Defensive"]
+//#define COOPR_RECONREP_BEHAVIOUR_OPTIONS ["Patrol", "Combat", "Defensive"]
+#define COOPR_RECONREP_BEHAVIOUR_OPTIONS ["Defensive"]
 
 #include "\x\coopr\addons\core\script_macros.hpp"

3da31892cab3c4b0a440ab660d27d2138e21058d
xetra11 <xetra11@outlook.com>
2019-08-23 11:49:21 +0200

    refactor(rpg): wip rpg attributes
    COOPR-24

--- addons/rpg/Cfg3DEN.hpp
+++ addons/rpg/Cfg3DEN.hpp
@@ -1,21 +1,39 @@
+class Combo;
+class ctrlMenuStrip;
+
+class LogLevelCombo: Combo {
+    class Items {
+        class None {
+            text = "None";
+            data = 0;
+        };
+        class Info {
+            text = "Info";
+            data = 1;
+        };
+        class Debug {
+            text = "Debug";
+            data = 2;
+        };
+    };
+};
+
 class Cfg3DEN {
     class Mission {
         class CoopR_RPG {
             displayName = "CoopR RPG Attributes";
-            //display = "Display3DENEditAttributesPreview"; // Optional - display for attributes window. Must have the same structure and IDCs as the default Display3DENEditAttributes
             class AttributeCategories {
                 class Logging {
                     class Attributes {
+                        class LogLevelCombo;
                         class LogLevel {
-                              property = "MyMissionAttributeUniqueID";
+                              control = "LogLevelCombo";
+                              property = "CoopRRPGLogLevel";
                               displayName = "Log Level";
                               description = "The log level to use for this addon";
+                              expression = "_this setVariable [""coopr_%s_rpg"", _value];";
+                              defaultValue = 0;
                               typeName = "NUMBER";
-                              class Values {
-                                  class opt_1 {name = "None"; value = 0;};
-                                  class opt_2 {name = "Info"; value = 1; default = 1;};
-                                  class opt_3 {name = "Debug"; value = 2;};
-                              };
                         };
                     };
                 };
@@ -23,7 +41,7 @@ class Cfg3DEN {
         };
     };
 };
-class ctrlMenuStrip;
+
 class display3DEN {
     class Controls {
         class MenuStrip: ctrlMenuStrip {
@@ -32,7 +50,7 @@ class display3DEN {
                     items[] += {"CoopR_RPG_Attributes"};
                 };
                 class CoopR_RPG_Attributes {
-                    text = "CoopR RPG Attributes";
+                    text = "CoopR RPG Settings";
                     action = "edit3DENMissionAttributes 'CoopR_RPG';";
                 };
             };

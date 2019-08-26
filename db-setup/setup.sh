f3f77db650a09b3e6a521176cac435f2fae08afd
Ricky 'diveyez' Neff <nemofbaltimore@gmail.com>
2019-08-26 12:17:39 -0700

    Create setup.sh

---
+++ db-setup/setup.sh
@@ -0,0 +1,3 @@
+# Install/Setup script for required parts to operate a Database and HQ for CoopR Mod
+
+** Script initially for Linux systems with intention to provide both Windows and MacOS versions**

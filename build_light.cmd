512a5c03ee4ab1e13625fd93d4d8fe63455986fd
xetra11 <xetra11@outlook.com>
2019-08-27 19:33:08 +0200

    chore(light): add CoopR light build scripts

---
+++ build_light.cmd
@@ -0,0 +1,21 @@
+:: This cmd script will build all addons into .pbo files and copies them
+:: into the @CoopR folder in the root directory
+
+SET armake64="tools\armake_w64.exe"
+SET validator="tools\sqf_validator.py"
+SET dest="@CoopRLight\addons"
+SET keys="@CoopRLight\keys"
+
+python %validator%
+
+rmdir /Q /S "@CoopRLight"
+mkdir %dest%
+mkdir %keys%
+
+%armake64% build -p addons\core %dest%\core.pbo
+%armake64% build -p addons\persistence %dest%\persistence.pbo
+%armake64% build -p addons\integration %dest%\integration.pbo
+%armake64% build -p addons\lobby %dest%\lobby.pbo
+
+copy mod.cpp @CoopRLight\mod.cpp
+copy keys\cooprlight.bikey @CoopRLight\keys\
--- fetch_key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rd private_key
-mkdir private_key
-git clone git@github.com:xetra11/coopr-private-bikey.git private_key/

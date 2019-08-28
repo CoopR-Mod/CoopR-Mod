f29446d69f5bc0eccecdab3c3d9c7e21f87d2249
Andrés Riquelme <andresfranco.rs@gmail.com>
2019-08-28 11:18:59 -0400

    feat(campsite): added  placeholder item desc COOPR-93

--- addons/campsite/cfgWeapons.hpp
+++ addons/campsite/cfgWeapons.hpp
@@ -5,9 +5,9 @@ class cfgWeapons{
 	class coopr_item_foldedTent : ItemCore{
 		scope = 2;
 		author = "Gilles";
-		displayName = "$STR.coopr.campsite.item.tent";
-		descriptionShort = "useless";
-		descriptionUse = "useless";
+		displayName = "$STR.coopr.campsite.item.name.tent";
+		descriptionShort = "$STR.coopr.campsite.item.desc.tent";
+		descriptionUse = "$STR.coopr.campsite.item.desc.tent";
 		picture = "\A3\EditorPreviews_F\Data\CfgVehicles\Land_Sleeping_bag_brown_folded_F.jpg";
 		model = "\A3\Structures_F\Civ\Camping\Sleeping_bag_brown_folded_F.p3d";
 		class ItemInfo: CBA_MiscItem_ItemInfo{
